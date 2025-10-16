a76dbe3393c1d1bc5243e4aa275f04c3439323e8
Antonio Figueiredo <antonio_figueiredo@web.de>
2025-10-16 09:43:35 +0200

    Some more changes

--- .scripts/buildpkg.sh
+++ .scripts/buildpkg.sh
@@ -25,7 +25,6 @@ cd ${WORKING_DIR}/${SRC_DIR_NAME}
 git archive HEAD | bzip2 > ../plxsdk_0.1.0.orig.tar.bz2
 
 # Add deb-src entries
-#sed -n '/^deb\s/s//deb-src /p' /etc/apt/debian.sources > /etc/apt/sources.list.d/deb-src.list # debug: different in new debian versions
 if [ -f /etc/apt/sources.list ]; then
     sed -n '/^deb\s/s//deb-src /p' /etc/apt/sources.list > /etc/apt/sources.list.d/deb-src.list
 elif [ -d /etc/apt/sources.list.d ]; then
