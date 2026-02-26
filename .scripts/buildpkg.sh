c1b9c31c3777dd054fd1bba913b0a9e672d7b6d6
Antonio Figueiredo <antonio_figueiredo@web.de>
2026-02-26 23:12:22 +0100

    Test signing

--- .scripts/buildpkg.sh
+++ .scripts/buildpkg.sh
@@ -9,30 +9,59 @@ export CCACHE_DIR=${TOP_DIR}/.ccache
 export WORKING_DIR=${TOP_DIR}/debian/output
 export SRC_DIR_NAME=source_dir
 
+#echo "deb http://deb.debian.org/debian testing main" > /etc/apt/sources.list
 
+# # changes start
+# apt-get update
+# git config --global --add safe.directory /github/workspace/source_dir # TODO is this realy needed?
 git config --global --add safe.directory /github/workspace/debian/output/source_dir
+# # changes end
 
 mkdir -p ${WORKING_DIR}
 cp -ra ${TOP_DIR}/${SRC_DIR_NAME} ${WORKING_DIR}
-
-# Import GPG key if provided
-if [ -n "$GPG_PRIVATE_KEY" ]; then
-    echo "$GPG_PRIVATE_KEY" | gpg --batch --import
-fi
 
 # Enter source package dir
 cd ${WORKING_DIR}/${SRC_DIR_NAME}
 
 # Build orig-tar.
-git archive HEAD | bzip2 > ../${PROJECT_NAME}_0.1.0.orig.tar.bz2
+git archive HEAD | bzip2 > ../plxsdk_0.1.0.orig.tar.bz2
 
 # Add deb-src entries
+# # changes start
+# if [ -f /etc/apt/sources.list ]; then
+#     sed -n '/^deb\s/s//deb-src /p' /etc/apt/sources.list > /etc/apt/sources.list.d/deb-src.list
+# elif [ -d /etc/apt/sources.list.d ]; then
+#     if [ "$(ls -A /etc/apt/sources.list.d/*.list 2>/dev/null)" ]; then
+#         for file in /etc/apt/sources.list.d/*.list; do
+#             sed -n '/^deb\s/s//deb-src /p' "$file" >> /etc/apt/sources.list.d/deb-src.list
+#         done
+#     fi
+#     if [ "$(ls -A /etc/apt/sources.list.d/*.sources 2>/dev/null)" ]; then
+#         for file in /etc/apt/sources.list.d/*.sources; do
+#             awk '/^Types:/ {if ($2 ~ /deb$/) print $0}' "$file" >> /etc/apt/sources.list.d/deb-src.list
+#         done
+#     fi
+# fi
+# # changes end
+# sed -n '/^deb\s/s//deb-src /p' /etc/apt/sources.list > /etc/apt/sources.list.d/deb-src.list # new
+# sed -n '/^deb\s/s//deb-src /p' /etc/apt/sources.list.d > /etc/apt/sources.list.d/deb-src.list # debug
 > /etc/apt/sources.list.d/deb-src.list # empty the file first
 for file in /etc/apt/sources.list.d/*.list; do
     sed -n '/^deb\s/s//deb-src /p' "$file" >> /etc/apt/sources.list.d/deb-src.list # >> appends instead of overwriting
 done
 
-# Update package lists and install build dependencies
+# # added dh-kms, dkms, linux-libc-dev
+# apt-get update && eatmydata apt-get install --no-install-recommends -y \
+#      aptitude \
+#      devscripts \
+#      ccache \
+#      equivs \
+#      build-essential \
+#      dh-dkms \
+#      dkms \
+#      linux-libc-dev
+
+#new
 apt-get update && eatmydata apt-get install --no-install-recommends -y \
      aptitude \
      devscripts \
@@ -40,8 +69,10 @@ apt-get update && eatmydata apt-get install --no-install-recommends -y \
      equivs \
      build-essential
 
+# #changes start
 # Install build dependencies directly
 eatmydata mk-build-deps --install --remove --tool "apt-get -o Debug::pkgProblemResolver=yes -y" debian/control
+# # changes end
 
 # Generate ccache links
 dpkg-reconfigure ccache
@@ -52,16 +83,7 @@ ccache -z
 
  # Create build user and fix permissions
 useradd buildci
-
-# Copy GPG keyring to buildci user
-mkdir -p /home/buildci/.gnupg
-chown -R buildci:buildci /home/buildci/.gnupg
-chmod 700 /home/buildci/.gnupg
-if [ -n "$GPG_PRIVATE_KEY" ]; then
-    su buildci -c "echo \"$GPG_PRIVATE_KEY\" | gpg --batch --import"
-fi
-
-chown -R buildci:buildci ${WORKING_DIR} ${CCACHE_DIR}
+chown -R buildci. ${WORKING_DIR} ${CCACHE_DIR}
 
 # Define buildlog filename
 BUILD_LOGFILE_SOURCE=$(dpkg-parsechangelog -S Source)
@@ -73,7 +95,7 @@ BUILD_LOGFILE="${WORKING_DIR}/${BUILD_LOGFILE_SOURCE}_${BUILD_LOGFILE_VERSION}_$
 # Build package as user buildci
 ls -la
 ls -la ..
-su buildci -c "eatmydata dpkg-buildpackage -kBD78A430515E1D36 ${DB_BUILD_PARAM}" |& OUTPUT_FILENAME=${BUILD_LOGFILE} filter-output
+su buildci -c "eatmydata dpkg-buildpackage ${DB_BUILD_PARAM}" |& OUTPUT_FILENAME=${BUILD_LOGFILE} filter-output
 
 ls -la
 find
